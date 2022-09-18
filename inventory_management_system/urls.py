7365a359020a4c9af9eabc0abb990562cf8ccfff
jmemorgzy <jamiemorgan2013@gmail.com>
2022-09-18 20:12:30 +0100

    implemented new login, logout and register pages

--- inventory_management_system/urls.py
+++ inventory_management_system/urls.py
@@ -2,12 +2,18 @@
 inventory_management_system URL Configuration
 """
 
+
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
+
+from users import views as user_views
 
 urlpatterns = [
     path("", include("home.urls")),
     path("inventory/", include('inventory.urls')),
     path('admin/', admin.site.urls),
-    path("accounts/", include("django.contrib.auth.urls")),
+    path('register/', user_views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
---
+++ users/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class UserRegistrationForm(UserCreationForm):
+    first_name = forms.CharField(max_length=101)
+
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'password1', 'password2']
