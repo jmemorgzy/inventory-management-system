a8165f72fa1b986b00dd21fcb23a99361e42fe73
jmemorgzy <jamiemorgan2013@gmail.com>
2022-09-19 17:01:39 +0100

    a weird place that needs fixing, a lot of fixing.

--- home/views.py
+++ home/views.py
@@ -1,6 +1,9 @@
+from django.contrib.auth import get_user_model
 from django.shortcuts import render
 
 
 def home(request):
-    context = {}
+    user = get_user_model()
+    users = user.objects.all()
+    context = {'users': users}
     return render(request, "home.html", context=context)
